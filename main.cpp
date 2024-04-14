3643312bbbdb1d44058020bc25f23e04e5ead545
dny_esr <kyspanova@icloud.com>
2024-04-14 11:06:29 +0400

    add main

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <mqueue.h>
+#include <cerrno>
 
 using namespace std;
 
@@ -37,60 +38,50 @@ void secondPlayer(int N, mqd_t mq) {
     std::stringstream ss;
 
     // Получаем секретное число из очереди сообщений
-    char buffer[MAX_MESSAGE_SIZE ];
-    mq_receive(mq, *buffer, MAX_MESSAGE_SIZE, NULL);
-    cout << "Сообщение было принято вторым процессом "<< endl;
-    secretNumber = atoi(buffer);
-    cout << secretNumber << endl;
-    // Играем, пока не угадаем число или не достигнем максимального числа попыток
-    do {
-        guess = rand() % N + 1;
-        cout << "Попытка: " << guess << endl;
-        attempts++;
-    } while (guess != secretNumber && attempts < MAX_GUESSES);
-
-    // Выводим статистику игры
-    if (guess == secretNumber) {
-        cout << "Число попыток: " << attempts << endl;
-    } else {
-        cout << "Не удалось угадать число за " << MAX_GUESSES << " попыток." << endl;
+    char buffer[MAX_MESSAGE_SIZE] = "";
+    ssize_t bytes = 0;
+    bytes = mq_receive(mq, buffer, MAX_MESSAGE_SIZE, NULL);
+    if(bytes < 0){
+        cerr << "Ошибка при приеме сообщения: " << strerror(errno) << endl;
     }
+    else{
+        cout << "Сообщение было принято вторым процессом "<< endl;
+        secretNumber = atoi(buffer);
+        cout << secretNumber << endl;
+        // Играем, пока не угадаем число или не достигнем максимального числа попыток
+        do {
+            guess = rand() % N + 1;
+            cout << "Попытка: " << guess << endl;
+            attempts++;
+        } while (guess != secretNumber && attempts < MAX_GUESSES);
+        // Выводим статистику игры
+        if (guess == secretNumber) {
+            cout << "Число попыток: " << attempts << endl;
+        } else {
+            cout << "Не удалось угадать число за " << MAX_GUESSES << " попыток." << endl;
+        }
+    }    
 }
 
 int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        cerr << "Использование: " << argv[0] << " N" << endl;
-        return EXIT_FAILURE;
-    }
-
-    int N = atoi(argv[1]);
-
-    // Создаем очередь сообщений
-    mqd_t mq = mq_open("/number_guessing_queue", O_RDWR | O_CREAT, 0600, NULL);
-    if (mq == (mqd_t)-1) {
-        cerr << "Ошибка при создании очереди сообщений" << endl;
-        return EXIT_FAILURE;
-    }
-
+    int N = 3;
     pid_t pid = fork();
-
-    if (pid < 0) {
-        cerr << "Ошибка при создании второго процесса" << endl;
-        return EXIT_FAILURE;
-    } else if (pid == 0) {
+    if (pid) {
+        mqd_t mq = mq_open("/queue", O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
         // Второй процесс (второй игрок)
+        cout << mq << endl;
         secondPlayer(N, mq);
-    } else {
-        // Первый процесс (первый игрок)
-        firstPlayer(N, mq);
-
-        // Ждем завершения второго процесса
-        wait(NULL);
-
-
-        // Закрываем и удаляем очередь сообщений
+        int stat;
+        wait(&stat);
         mq_close(mq);
-        mq_unlink("/number_guessing_queue");
+        mq_unlink("/queue");
+    }
+    else{
+        mqd_t mq = mq_open("/queue", O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+        cout << mq << endl;
+        firstPlayer(N, mq);
+        mq_close(mq);
+        mq_unlink("/queue");
     }
 
     return EXIT_SUCCESS;
